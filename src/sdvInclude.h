ea8850824395b8d4ff450f75e6aaa428d7c97304
unknown <A013425@naton.cn>
2022-04-15 10:11:40 +0800

    cmkaeLists自动查找

--- src/sdvInclude.h
+++ src/sdvInclude.h
@@ -5,10 +5,3 @@
 #include "vtkCellPicker.h"
 #include "vtkCommand.h"
 #include "vtkDICOMImageReader.h"
-
-
-//itk
-#include "itkSymmetricSecondRankTensor.h"
-#include "itkGDCMImageIO.h"
-#include "itkImage.h"
-#include "itkImageSeriesReader.h"
